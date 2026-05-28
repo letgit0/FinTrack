8c727929e28818c74a71e2a5731e5aacbc02c162
Smita <smita16025@gmail.com>
2026-05-28 15:06:42 +0530

    Setup MongoDB database and Server Connection

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import type { Express } from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: ".env.local" });
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const MONGOURI = process.env.MONGOURI;
+
+if (!MONGOURI) {
+    throw new Error("MONGOURI is not defined in .env file");
+}
+
+mongoose.connect(MONGOURI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
