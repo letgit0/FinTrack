cb0e111931bd9638e1d48a2c4b615a4b15dee08b
Smita <smita16025@gmail.com>
2026-05-29 22:58:15 +0530

    Resolve vercel build failure

--- client/src/pages/dashboard/RecordForm.tsx
+++ client/src/pages/dashboard/RecordForm.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import type{ ChangeEvent, FormEvent } from "react";
+import {useState } from "react";
 import { useUser } from "@clerk/clerk-react";
 import { useRecords } from "../contexts/record-context";
 
